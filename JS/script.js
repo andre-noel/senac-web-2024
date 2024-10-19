3b88685261e3af4cc64eece38ac04da1fa8b7cbf
Andre Noel <andre.noel@docente.pr.senac.br>
2024-10-18 22:05:09 -0300

    Criação da calculadora

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,18 @@
-const num1 = document.querySelector("#num1");
-const num2 = document.querySelector("#num2");
-const resultado = document.querySelector("#resultado");
-const botaoSoma = document.querySelector("#botaoSoma");
+// Captura a div onde vou colocar os botões
+const botoes = document.querySelector('.botoes');
 
-botaoSoma.addEventListener("click", function () {
-    resultado.innerHTML = parseInt(num1.value) + parseInt(num2.value);
-});
+// Cria um array com os rótulos dos botões
+const rotulosBotoes = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'CE', '0', '=', '+'];
 
+// Faz um looping para criar os botões
+// O looping vai de 0 até o último elemento do array
+for (let index = 0; index < rotulosBotoes.length; index += 1) {
+    // Cria um elemento button e salva na variável botaoNovo
+    const botaoNovo = document.createElement('button');
+
+    // Escreve o rótulo dentro do botão
+    botaoNovo.innerHTML = rotulosBotoes[index];
+
+    // Adiciona o botão na div botoes
+    botoes.appendChild(botaoNovo);
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # senac-web-2024
 Repositório para a disciplina de Fundamentos de Desenvolvimento Web
+
+Acesse https://github.com/andre-noel/senac-web-2024
+Execute:
+
+`git clone https://github.com/andre-noel/senac-web-2024.git`
